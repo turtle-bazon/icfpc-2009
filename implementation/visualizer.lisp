9e63d40def88f38beff87430143b88fe358f514b
Kalyanov Dmitry <Kalyanov.Dmitry@gmail.com>
2009-06-27 23:30:15 +0400

    рефакторинг визуализации

--- implementation/visualizer.lisp
+++ implementation/visualizer.lisp
@@ -9,8 +9,14 @@
 
 (defstruct visualizer
   (xs (make-array 0 :element-type 'double-float :adjustable t :fill-pointer t))
-  (ys (make-array 0 :element-type 'double-float :adjustable t :fill-pointer t))
-  target-radius)
+  (ys (make-array 0 :element-type 'double-float :adjustable t :fill-pointer t)))
+
+(defstruct (hohmann-visualizer (:include visualizer)) target-radius)
+
+(defun problem->visualizer (problem)
+  (case problem
+    ((1001 1002 1003 1004) (make-hohmann-visualizer))
+    (t (make-visualizer))))
 
 (defun make-steps-finish-predicate (steps)
   (let ((counter 0))
@@ -28,7 +34,7 @@
                       (max-steps 50000)
                       solution)
   (let* ((simulator (create-simulator obf-path))
-         (visualizer (make-visualizer))
+         (visualizer (problem->visualizer configuration))
          (dumper (simulator::make-dumper))
          (control-structure (make-control-structure :vm-semaphore (sb-thread:make-semaphore)
                                                     :ctl-semaphore (sb-thread:make-semaphore)
@@ -69,13 +75,18 @@
     (when solution (simulator::stop-dumper dumper))
     (visualizer-save visualizer result-path)))
 
-(defun visualizer-collect (visualizer simulator)
+(defgeneric visualizer-collect (visualizer simulator))
+
+(defmethod visualizer-collect ((visualizer visualizer) simulator)
   (let* ((info (simulator-info simulator :hohmann))
          (our-x (getf info :our-x))
          (our-y (getf info :our-y)))
     (vector-push-extend our-x (visualizer-xs visualizer))
-    (vector-push-extend our-y (visualizer-ys visualizer))
-    (setf (visualizer-target-radius visualizer)
+    (vector-push-extend our-y (visualizer-ys visualizer))))
+
+(defmethod visualizer-collect :after ((visualizer hohmann-visualizer) simulator)
+  (let* ((info (simulator-info simulator :hohmann)))
+    (setf (hohmann-visualizer-target-radius visualizer)
           (getf info :target-orbit-radius))))
 
 (defparameter *zoom* 9.5d4)
@@ -85,32 +96,33 @@
    (+ (expt x 2)
       (expt y 2))))
 
-(defun visualizer-save (visualizer result-path)
-  (let ((dx (+ (/ 6.357d6 *zoom*) 100)))
-    (flet ((coord (c) (format nil "~F" (+ dx (/ c *zoom*)))))
-      (with-open-file (stream result-path :direction :output :if-exists :supersede)
-        (format stream "<?xml version='1.0' encoding='UTF-8'?>~%")
-        (format stream "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'>~%")
-        (format stream "<circle cx='~A' cy='~A' r='~F' style='stroke: #000000; fill:#00ff00;' />~%" (coord 0) (coord 0) (/ 6.357d6 *zoom*))
-        (format stream "<circle cx='~A' cy='~A' r='~F' style='stroke: #ff0000; fill:transparent;' />~%" (coord 0) (coord 0)
-                (/ (visualizer-target-radius visualizer) *zoom*))
-        (format stream "<circle cx='~A' cy='~A' r='~F' style='stroke: #0000ff; fill:transparent;' />~%" (coord 0) (coord 0)
-                (/ (vector-length (aref (visualizer-xs visualizer) 0)
-                                  (aref (visualizer-ys visualizer) 0)) *zoom*))
-        (iter (for x in-vector (visualizer-xs visualizer))
-              (for y in-vector (visualizer-ys visualizer))
-              (for p-x previous x)
-              (for p-y previous y)
-              (unless (first-iteration-p)
-                (format stream "<line style='stroke: #000000' x1='~A' x2='~A' y1='~A' y2='~A' />~%" (coord p-x) (coord x) (coord p-y) (coord y))))
-        (format stream "</svg>~%")))))
+(defgeneric visualizer-save (visualizer result-path))
 
-(defun make-initial-thrust-function (v-x v-y)
-  (let (returned)
-    (lambda (simulator)
-      (declare (ignore simulator))
-      (if returned
-          (values 0 0)
-          (progn
-            (setf returned t)
-            (values v-x v-y))))))
+(defvar *dx*)
+
+(defvar *stream*)
+
+(defun coord (c)
+  (format nil "~F" (+ *dx* (/ c *zoom*))))
+
+(defmethod visualizer-save :around ((visualizer visualizer) result-path)
+  (let ((*dx* (+ (/ 6.357d6 *zoom*) 100)))
+    (with-open-file (*stream* result-path :direction :output :if-exists :supersede)
+      (format *stream* "<?xml version='1.0' encoding='UTF-8'?>~%")
+      (format *stream* "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'>~%")
+      (format *stream* "<circle cx='~A' cy='~A' r='~F' style='stroke: #000000; fill:#00ff00;' />~%" (coord 0) (coord 0) (/ 6.357d6 *zoom*))
+      (iter (for x in-vector (visualizer-xs visualizer))
+            (for y in-vector (visualizer-ys visualizer))
+            (for p-x previous x)
+            (for p-y previous y)
+            (unless (first-iteration-p)
+              (format *stream* "<line style='stroke: #000000' x1='~A' x2='~A' y1='~A' y2='~A' />~%" (coord p-x) (coord x) (coord p-y) (coord y))))
+      (call-next-method)
+      (format *stream* "</svg>~%"))))
+
+(defmethod visualizer-save ((visualizer hohmann-visualizer) resul-path)
+  (format *stream* "<circle cx='~A' cy='~A' r='~F' style='stroke: #ff0000; fill:transparent;' />~%" (coord 0) (coord 0)
+                (/ (hohmann-visualizer-target-radius visualizer) *zoom*))
+        (format *stream* "<circle cx='~A' cy='~A' r='~F' style='stroke: #0000ff; fill:transparent;' />~%" (coord 0) (coord 0)
+                (/ (vector-length (aref (visualizer-xs visualizer) 0)
+                                  (aref (visualizer-ys visualizer) 0)) *zoom*)))
