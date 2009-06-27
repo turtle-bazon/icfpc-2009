96be27e2b5f9a0c49dc14b12018997efc5441771
turtle <turtle@bazon.ru>
2009-06-27 12:29:21 +0400

    Добавил лог по проблеме хохмана.

--- implementation/start.lisp
+++ implementation/start.lisp
@@ -1,15 +1,26 @@
 (require :simulator "simulator")
+(require :simulator "controller")
 
 (in-package :simulator)
 
-(defun stype (number)
-  (ecase number
-    (1 :hohmann)
-    (4 :meet-and-greet)))
+(defun problem-number (problem)
+  (ecase problem
+    (:hohmann 1)
+    (:meet-ang-greet 2)))
 
-(defun problem (number scenario)
-  (let ((simulator (create-simulator (format nil "../task/bin~a.obf" number))))
+(defun display-hohmann-info (info)
+  (format t "R:~a, X:~a, Y:~a, F:~a~%"
+	  (distance-to-earth (getf info :our-x) (getf info :our-y))
+	  (getf info :our-x)
+	  (getf info :our-y)
+	  (getf info :fuel-remaining)
+	  ))
+
+(defun problem (problem scenario)
+  (let ((simulator (create-simulator (format nil "../task/bin~a.obf" (problem-number problem)))))
     (configure-simulator simulator scenario)
-    (dotimes (i 10)
+    (dotimes (i 100)
       (step-simulator simulator 0 0)
-      (format t "~a~%" (simulator-info simulator (stype number))))))
+      (ecase problem
+	(:hohmann (display-hohmann-info (simulator-info simulator problem))))
+      )))
