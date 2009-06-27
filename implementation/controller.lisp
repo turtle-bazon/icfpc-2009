5a6ded880ed452eb5a126f815c4a3779ace5c0a4
turtle <turtle@bazon.ru>
2009-06-27 11:43:49 +0400

    Тут будет контроллер по идее.

---
+++ implementation/controller.lisp
@@ -0,0 +1,15 @@
+(in-package :simulator)
+
+(defconstant +mass-earth+ (* 6 (expt 10 24)))
+
+(defconstant +radius-earth+ (* 6.357 (expt 10 6)))
+
+(defconstant +gravity-constant+ (* 6.67428 (expt 10 -11)))
+
+(defun distance-between-bodies (sx1 sy1 sx2 sy2)
+  (sqrt
+    (+ (expt (- sx2 sx1) 2)
+       (expt (- sy2 sy1) 2))))
+
+(defun distance-to-earth (sx sy)
+  (distance-between-bodies sx sy 0 0))
